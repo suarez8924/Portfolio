5d66a390f17db760f1ab465f3fbf8f0699f6647c
Javier Suarez <suarezjav89@gmail.com>
2018-04-18 00:47:39 -0300

    added proyects

--- Gorillaz/js/script.js
+++ Gorillaz/js/script.js
@@ -2,62 +2,71 @@
 var modal = document.getElementById('myModal');
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
-var img1 = document.getElementById('myImg1');
-var img2 = document.getElementById('myImg2');
-var img3 = document.getElementById('myImg3');
-var img4 = document.getElementById('myImg4');
-var img5 = document.getElementById('myImg5');
-var img6 = document.getElementById('myImg6');
-var img7 = document.getElementById('myImg7');
-var img8 = document.getElementById('myImg8');
-var img9 = document.getElementById('myImg9');
-var modalImg = document.getElementById("img01");
+// var img1 = document.getElementById('myImg1');
+// var img2 = document.getElementById('myImg2');
+// var img3 = document.getElementById('myImg3');
+// var img4 = document.getElementById('myImg4');
+// var img5 = document.getElementById('myImg5');
+// var img6 = document.getElementById('myImg6');
+// var img7 = document.getElementById('myImg7');
+// var img8 = document.getElementById('myImg8');
+// var img9 = document.getElementById('myImg9');
+var image = document.getElementsByClassName('image')
+var modalImg = image[0]//document.getElementById("img01");
 var captionText = document.getElementById("caption");
-img1.onclick = function(){
-    modal.style.display = "block";
-    modalImg.src = this.src;
-    captionText.innerHTML = this.alt;
+
+for(var i = 0; i<image.length;i++){
+    image[0].onclick = function(){
+        modal.style.display = "block";
+        modalImg.src = this.src;
+        captionText.innerHTML = this.alt;
+    }        
 }
-img2.onclick = function(){
-    modal.style.display = "block";
-    modalImg.src = this.src;
-    captionText.innerHTML = this.alt;
-}
-img3.onclick = function(){
-    modal.style.display = "block";
-    modalImg.src = this.src;
-    captionText.innerHTML = this.alt;
-}
-img4.onclick = function(){
-    modal.style.display = "block";
-    modalImg.src = this.src;
-    captionText.innerHTML = this.alt;
-}
-img5.onclick = function(){
-    modal.style.display = "block";
-    modalImg.src = this.src;
-    captionText.innerHTML = this.alt;
-}
-img6.onclick = function(){
-    modal.style.display = "block";
-    modalImg.src = this.src;
-    captionText.innerHTML = this.alt;
-}
-img7.onclick = function(){
-    modal.style.display = "block";
-    modalImg.src = this.src;
-    captionText.innerHTML = this.alt;
-}
-img8.onclick = function(){
-    modal.style.display = "block";
-    modalImg.src = this.src;
-    captionText.innerHTML = this.alt;
-}
-img9.onclick = function(){
-    modal.style.display = "block";
-    modalImg.src = this.src;
-    captionText.innerHTML = this.alt;
-}
+// img1.onclick = function(){
+//     modal.style.display = "block";
+//     modalImg.src = this.src;
+//     captionText.innerHTML = this.alt;
+// }
+// img2.onclick = function(){
+//     modal.style.display = "block";
+//     modalImg.src = this.src;
+//     captionText.innerHTML = this.alt;
+// }
+// img3.onclick = function(){
+//     modal.style.display = "block";
+//     modalImg.src = this.src;
+//     captionText.innerHTML = this.alt;
+// }
+// img4.onclick = function(){
+//     modal.style.display = "block";
+//     modalImg.src = this.src;
+//     captionText.innerHTML = this.alt;
+// }
+// img5.onclick = function(){
+//     modal.style.display = "block";
+//     modalImg.src = this.src;
+//     captionText.innerHTML = this.alt;
+// }
+// img6.onclick = function(){
+//     modal.style.display = "block";
+//     modalImg.src = this.src;
+//     captionText.innerHTML = this.alt;
+// }
+// img7.onclick = function(){
+//     modal.style.display = "block";
+//     modalImg.src = this.src;
+//     captionText.innerHTML = this.alt;
+// }
+// img8.onclick = function(){
+//     modal.style.display = "block";
+//     modalImg.src = this.src;
+//     captionText.innerHTML = this.alt;
+// }
+// img9.onclick = function(){
+//     modal.style.display = "block";
+//     modalImg.src = this.src;
+//     captionText.innerHTML = this.alt;
+// }
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
